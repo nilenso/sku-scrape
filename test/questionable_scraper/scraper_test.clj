8257dd50abb6be9f5b9bf09354cbb8ba5f36dd75
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-11-14 11:18:20 +0530

    Fix scraper test: remove thread/sleep

--- test/questionable_scraper/scraper_test.clj
+++ test/questionable_scraper/scraper_test.clj
@@ -6,14 +6,16 @@
 (use-fixtures :once setup-once)
 (use-fixtures :each init-state)
 
+(def sample-sku "Washing Machine")
+
 (deftest scrape-tests
   (testing "Task should run only once for n concurrent calls with same sku"
     (let [call-count 100
           counter    (atom 0)]
-      (with-redefs [scraper/run-scrapers (fn [_ _]
+      (with-redefs [scraper/run-scrapers (fn [sku p]
                                            (swap! counter inc)
-                                           (Thread/sleep 300))]
-        (pmap scraper/scrape (repeat 100 "Washing Machine"))
-        (Thread/sleep 600)
+                                           (Thread/sleep 300)
+                                           (deliver p (str "Price of" sku)))]
+        (doall (pmap scraper/scrape (repeat 100 sample-sku)))
         ;; Check if count of pmap is = call-count
         (is (= 1 @counter))))))
